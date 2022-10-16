e50deb842f7d293743e25c63a59879ffcf2e2dca
Louis <sorrenti.l@northeastern.edu>
2022-10-16 19:53:18 -0400

    Fixed search icon

--- public/tuiter/ExploreComponent/ExploreComponent.js
+++ public/tuiter/ExploreComponent/ExploreComponent.js
@@ -3,11 +3,11 @@ import PostSummaryList from "../PostSummaryList/index.js";
 const ExploreComponent = () => {
     return(`
         <div>
-            <i class="position-absolute text-secondary m-3 fas fa-search"></i>
+            <i class="position-absolute text-secondary ms-3 mt-2 fas fa-search"></i>
             <input type="text"
                class="wd-form-control mb-2 rounded-pill"
                placeholder="Search Tuiter">
-            <a href="explore-settings.html"><i class="wd-icon-center mt-1 fa-2x fas fa-gear"></i></a>
+            <a href="explore-settings.html"><i class="wd-icon-center fa-2x fas fa-gear"></i></a>
         </div>
         <div class="d-none d-md-block">
             <ul class="nav mb-2 nav-tabs">
